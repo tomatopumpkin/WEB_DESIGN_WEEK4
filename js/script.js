c9398a5293976051aef7c9b7f6ed3ec39fedfcc4
benzkittisak <benzkittisak@stv.ac.th>
2022-01-25 13:31:25 +0700

    เสร็จแล้ว

---
+++ js/script.js
@@ -0,0 +1,12 @@
+document.getElementById(
+  "foot01"
+).innerHTML = `<p>&copy; ${new Date().getFullYear()} All rights Reserved</p>`;
+
+
+document.getElementById("nav01").innerHTML = `
+  <ul id="menu">
+    <li><a href="index.html">Home</a></li>
+    <li><a href="Customers.html">Data</a></li>
+    <li><a href="about.html">About</a></li>
+  </ul>
+`
